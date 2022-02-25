0f95faf14568c592f4b78915727130dc6dfa0a6a
J Lee <jay.j.lee.1@gmail.com>
2022-02-25 12:10:24 -0500

    commit tree d9f5f7de9729b00c174f2b8d25106fb583d4d081

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,30 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    # render json: item, only: [:id, :name, :description, :price, :user_id]
+    render json: item, except: [:created_at, :updated_at]
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
